e95f1be991066702a5ab2c19ecb25b5dd98fc7bd
Patrick Perini <pcperini@gmail.com>
2015-02-27 13:01:54 -0800

    Initial Commit

---
+++ supercluster/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  supercluster
+//
+//  Created by Patrick Perini on 2/27/15.
+//  Copyright (c) 2015 perini-hestin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
