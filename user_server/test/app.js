338121f53ba9baea8b49824a784784d68ffe0850
Patrick Perini <pcperini@gmail.com>
2015-02-28 11:07:14 -0800

    nooooode

--- user_server/test/app.js
+++
@@ -1,7 +0,0 @@
-var ioc = require("socket.io-client");
-var client = ioc("ws://localhost:3000");
-
-client.emit('location', 1, 2);
-console.log("FUCK");
-client.emit('left', 1, 2);
-client.emit('reset');
